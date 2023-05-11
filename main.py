35004a03eb797b67b8d459a35f6d103002dc2f72
KatePonomarenko <52855607+artemqq5@users.noreply.github.com>
2023-05-11 12:43:58 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook_handler():
+    data = request.get_json() or request.form.to_dict()
+
+    print(data)
+    return 'OK', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
