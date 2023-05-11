9450171461ee3a77a7713188a798f6ffc5b7afe3
KatePonomarenko <52855607+artemqq5@users.noreply.github.com>
2023-05-11 16:59:03 +0300

    fix 8.0

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from gevent.pywsgi import WSGIServer
+from trello import TrelloClient
 import requests
 
 from private_config import local_telegram_token, server_telegram_token, list_tech_done_local, list_creo_done_local, \
-    list_tech_done, list_creo_done
+    list_tech_done, list_creo_done, idList_creo_test, idList_tech_test, idList_tech, idList_creo, local_api_key_trello, \
+    local_token_trello, local_secret_trello, server_api_key_trello, server_token_trello, server_secret_trello
 
 app = Flask(__name__)
 
@@ -13,10 +15,26 @@ if DEBUG_MODE:
     URL_MESSAGE = f"https://api.telegram.org/bot{local_telegram_token}/sendMessage"
     list_tech = list_tech_done_local
     list_creo = list_creo_done_local
+    list_from_tech = idList_tech_test
+    list_from_creo = idList_creo_test
+    API_KEY_TRELLO = local_api_key_trello
+    TOKEN_TRELLO = local_token_trello
+    API_SECRET_TRELLO = local_secret_trello
 else:
     URL_MESSAGE = f"https://api.telegram.org/bot{server_telegram_token}/sendMessage"
     list_tech = list_tech_done
     list_creo = list_creo_done
+    list_from_tech = idList_tech
+    list_from_creo = idList_creo
+    API_KEY_TRELLO = server_api_key_trello
+    TOKEN_TRELLO = server_token_trello
+    API_SECRET_TRELLO = server_secret_trello
+
+clientTrelloApi = TrelloClient(
+    api_key=API_KEY_TRELLO,
+    api_secret=API_SECRET_TRELLO,
+    token=TOKEN_TRELLO
+)
 
 
 @app.route('/webhook', methods=['POST', 'GET'])
@@ -24,26 +42,12 @@ def webhook_handler():
     if request.method == 'POST':
         try:
             data = request.get_json()
-            print(data)
 
-            if data['action']['data']['card']['idList'] == list_creo:
-                print('creative')
-            elif data['action']['data']['card']['idList'] == list_tech:
-                print('tech')
+            if data['action']['data']['card']['idList'] in (list_creo, list_tech):
+                if data['action']['display']['entities']['listBefore'] in (list_from_creo, list_from_tech):
+                    card_id = data['action']['data']['card']['id']
+                    get_card(card_id)
 
-            card_label = data['action']['data']['card']['name']
-            card_id = data['action']['data']['card']['id']
-
-            jsonDataPass = {
-                "chat_id": "6002568864",
-                "text": f"{card_label} | {card_id}"
-            }
-
-            result = requests.request(method='POST', url=URL_MESSAGE, data=jsonDataPass)
-            print(result)
-
-            print(card_label)
-            print(card_id)
         except Exception as e:
             print("exception when getting data POST")
             print(f"exception {e}")
@@ -52,6 +56,21 @@ def webhook_handler():
 
     return 'OK', 200
 
+
+def get_card(id_card):
+    # jsonDataPass = {
+    #     "chat_id": "6002568864",
+    #     "text": f"{'card_label'} | {'card_id'}"
+    # }
+
+    print(clientTrelloApi.get_card(id_card))
+
+    # result = requests.request(method='POST', url=URL_MESSAGE, data=jsonDataPass)
+    # print(result)
+
+
+# class Card:
+#     def __self__(self, ):
 
 if __name__ == '__main__':
     http_server = WSGIServer(("0.0.0.0", 5000), app)
