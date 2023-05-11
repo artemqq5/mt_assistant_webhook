1cba190e0252fc10e8c41e40d7816708c0b17b83
KatePonomarenko <52855607+artemqq5@users.noreply.github.com>
2023-05-11 20:45:22 +0300

    fix 12.0

---
+++ start_bot_v2.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+while True:
+    print('start webhook handler')
+    try:
+        os.system("python3 /home/ftpuser/webhook_handler/main.py")
+    except Exception as e:
+        print(f'exception in start: {e}') 
+    print('crash')
+    time.sleep(5)
+    
