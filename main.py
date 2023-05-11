7929e8dd8790d1324265db10ec7d6afd05c2b91a
KatePonomarenko <52855607+artemqq5@users.noreply.github.com>
2023-05-11 15:23:00 +0300

    add more functionality

--- main.py
+++ main.py
@@ -3,18 +3,41 @@ from gevent.pywsgi import WSGIServer
 import requests
 import json
 
+from private_config import local_telegram_token, server_telegram_token, list_tech_done_local, list_creo_done_local, \
+    list_tech_done, list_creo_done
+
 app = Flask(__name__)
 
+DEBUG_MODE = False
 
-@app.route('/webhook', methods=['POST'])
+if DEBUG_MODE:
+    URL_MESSAGE = f"https://api.telegram.org/bot{local_telegram_token}/sendMessage"
+    list_tech = list_tech_done_local
+    list_creo = list_creo_done_local
+else:
+    URL_MESSAGE = f"https://api.telegram.org/bot{server_telegram_token}/sendMessage"
+    list_tech = list_tech_done
+    list_creo = list_creo_done
+
+
+@app.route('/webhook', methods=['POST', 'GET'])
 def webhook_handler():
-    data = request.get_json() or request.form.to_dict()
+    if request.method == 'POST':
+        data = request.get_json()
+
+    # data_message = {
+    #     "chat_id": chat_id,
+    #     "text": text
+    # }
+    # requests.request('POST', URL_MESSAGE, data_message)
+
+    else:
+        data = "get request success"
 
     print(data)
     return 'OK', 200
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
     http_server = WSGIServer(("0.0.0.0", 5000), app)
     http_server.serve_forever()
