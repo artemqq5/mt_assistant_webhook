7799366c22d932b022dde6409600abc31b244837
artem <artemqq5@gmail.com>
2024-07-15 13:16:20 +0300

    Update start_handler.py

--- start_handler.py
+++ start_handler.py
@@ -4,7 +4,7 @@ import time
 while True:
     print('start webhook handler trello')
     try:
-        os.system("python3.10 /home/ftpuser/webhook_handler/main.py")
+        os.system("python3.10 /home/ftpuser/trello_webhook/main.py")
     except Exception as e:
         print(f'exception in start: {e}') 
     print('crash')
